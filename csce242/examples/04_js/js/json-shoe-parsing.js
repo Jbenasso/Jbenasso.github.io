ea75406dfb1f39c8388de39d4cf8f68a5f4afe08
Jbenasso <benassoj@email.sc.edu>
2025-10-14 15:05:17 -0400

    Refactor code structure for improved readability and maintainability

--- csce242/examples/04_js/js/json-shoe-parsing.js
+++ csce242/examples/04_js/js/json-shoe-parsing.js
@@ -1,55 +1,51 @@
 const getShoes = async() => {
-    const url = "https://jbenasso.github.io/json/shoes.json";
+    const url = "https://portiaportia.github.io/json/shoes.json";
 
     try {
         const response = await fetch(url);
         return response.json();
     }
-    catch (error) {
+    catch(error){
         console.log("sorry");
     }
 };
 
-const showShoes = async () => {
+const showShoes = async() => {
     const shoes = await getShoes();
     const shoeListDiv = document.getElementById("shoe-list");
 
-    
-    shoes.forEach((shoe)=> {
+    shoes.forEach((shoe)=>{
         //make a section for each shoe put all the data in, nicely formatted
         //then append it to the shoe list
-        const section = document.createElement("h3");
+        const section = document.createElement("section");
         section.classList.add("shoe");
 
         //heading
         const h3 = document.createElement("h3");
         section.append(h3);
-        h3.innerHTMl
+        h3.innerHTML = shoe.name;
 
         //brand
         const p = document.createElement("p");
         section.append(p);
         p.innerHTML = `Brand: ${shoe.brand}`;
 
-        //loop through reviews reviews and make a ul li list
+        //loop through reviews reviews and make ul li list
         const ul = document.createElement("ul");
+        section.append(ul);
 
-        shoe.reviews.forEach(review => {
+        shoe.reviews.forEach((review) => {
             const li = document.createElement("li");
             li.innerHTML = review;
             ul.append(li);
         });
-        section.append(ul);
-
 
         //image
-        const img = document.createElement("img");
-        section.append(img);
-        img.src = `https://jbenasso.github.io/json/images/${shoe.image}`;
-        
+        //const img = document.createElement("img");
+        //section.append(img);
+        //img.src = `https://portiaportia.github.io/json/${shoe.img}`;
 
         shoeListDiv.append(section);
-
     });
 };
 
